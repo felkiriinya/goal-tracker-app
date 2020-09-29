a49aff7bb2b9bc475482069507e203dfc173235b
felkiriinya <felkiriinya@gmail.com>
2020-09-29 08:55:54 +0300

    practised uses of services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
@@ -8,7 +9,8 @@ import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
-
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,10 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,21 +1,30 @@
+import { Quote } from '../quote-class/quote';
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../alert-service/alert.service';
 import { Goal } from '../goal';
-
-
+import { GoalService } from '../goal-service/goal.service';
+import { HttpClient } from '@angular/common/http';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
-    goals: Goal[] = [
-        new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-        new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-        new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-        new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-        new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-        new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
-      ];
+    goals:Goal[];
+    alertService: AlertService;
+    quote:Quote;
+    //  = [
+    //     new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
+    //     new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
+    //     new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
+    //     new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
+    //     new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+    //     new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+    //   ];
+
+    
+        
 
       toggleDetails(index){
         this.goals[index].showDescription = !this.goals[index].showDescription;
@@ -32,19 +41,40 @@ export class GoalComponent implements OnInit {
     
           if (toDelete){
             this.goals.splice(index,1)
+            this.alertService.alertMe("The goal has been deleted")
           }
         }
       }
       addNewGoal(goal){
-        let goalLength = this.goals.length;
-        goal.id = goalLength+1;
-        goal.completeDate = new Date(goal.completeDate)
+        // let goalLength = this.goals.length;
+        // goal.id = goalLength+1;
+        // goal.completeDate = new Date(goal.completeDate)
         this.goals.push(goal)
       }
-
-  constructor() { }
+      constructor(goalService:GoalService, alertService:AlertService, private quoteService:QuoteRequestService) {
+        this.goals = goalService.getGoals()
+        this.alertService = alertService;
+      }
+    
+  
 
   ngOnInit() {
+    this.quoteService.quoteRequest()
+    this.quote = this.quoteService.quote
+    //   interface ApiResponse{
+    //       author:string;
+    //       quote:string;
+    //   }
+    //   this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+    //   // Succesful API request
+    //   this.quote = new Quote(data.author, data.quote)
+    // },
+    // err=>{
+    //     this.quote =new Quote("Winston Churchill","Never never give up!")
+    //     console.log("An error occurred")
+    // }
+    // )
+
   }
 
 }
